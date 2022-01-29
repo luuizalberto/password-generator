04aaa51ec6c291155636e4c32af27c9980f804f2
Luiz Alberto <79115384+luuizalberto@users.noreply.github.com>
2022-01-29 15:54:52 -0300

    update
    função para gerar senha finalizado

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ let password = document.querySelector('#password')
 
 let containerPassword = document.querySelector('#container-password')
 
-// quais caracteres eu quero que possa gerar
-let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#%*" 
+// quais caracteres eu quero que possa gerar.
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#%*-" 
 let novaSenha = ""
 
-// Deixando dinâmico o tamanho da senha de acordo com a barra
+// Deixando dinâmico o tamanho da senha de acordo com a barra.
 sizePassword.innerHTML = sliderElement.value
 
 sliderElement.oninput = function(){
@@ -18,3 +18,14 @@ sliderElement.oninput = function(){
 }
 
 // Função para gerar a senha
+function generatePassword(){
+    
+    let pass = "" // variavel vazia pq vai receber os valores do for. A cada passada na variável charset ele vai adicionar um valor nesta variável de acordo com o tamanho que o usuário escolher.
+
+    // Vai começar em 0 --- e vai até o tamanho que tem na variável Charset --- vai passar enquanto i for menor que o valor do slider;
+    for(let i = 0, n = charset.length; i < sliderElement.value; ++i){ 
+        pass += charset.charAt(Math.floor(Math.random() * n)) // charAt é a posição --- Math.floor é para gerar um inteiro e Math.ramdom para gerar um aleatório
+    } 
+
+    console.log(pass)
+}
