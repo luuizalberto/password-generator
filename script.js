2babb508da176c310eecf0a4ad3f9b2d629045af
Luiz Alberto <79115384+luuizalberto@users.noreply.github.com>
2022-01-29 15:20:47 -0300

    inciando script

---
+++ script.js
@@ -0,0 +1,7 @@
+let sliderElement = document.getElementById('slider')
+let buttonElement = document.getElementById('#button')
+
+let sizePassword = document.querySelector('#valor')
+let password = document.querySelector('#password')
+
+let containerPassword = document.querySelector('#container-password')
